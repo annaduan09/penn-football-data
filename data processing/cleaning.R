cef064d2cc6bcbc7adfc08006187eecbe5975eee
Anna Duan <40662229+annaduan09@users.noreply.github.com>
2025-01-27 15:47:58 -0500

    new length parsers
    new length parsing functions, new NA handling, need to consolidate column names

--- data processing/cleaning.R
+++ data processing/cleaning.R
@@ -10,21 +10,25 @@ conflict_prefer("select", "dplyr")
 
 #### Functions ####
 
-
 # 6'1.5" height notation
-parse_ft_in <- function(ft_in_str) {
-  is_ft_in <- grepl("'", ft_in_str)
-  total_inches <- ifelse(
+parse_ft_in <- function(ft_in_vec) {
+  ft_in_vec <- gsub("''", "", ft_in_vec) 
+  ft_in_vec <- gsub("\"", "", ft_in_vec) 
+  is_ft_in <- grepl("'", ft_in_vec) 
+  
+  result <- ifelse(
     is_ft_in,
-    { parts <- strsplit(ft_in_str, "'")
-      feet <- as.numeric(sapply(parts, "[", 1))
-      inches <- as.numeric(sapply(gsub("\"", "", sapply(parts, "[", 2)), as.numeric))
-      (feet * 12) + inches },
-    as.numeric(ft_in_str)
+    {
+      parts <- strsplit(ft_in_vec[is_ft_in], "'")
+      feet <- as.numeric(sapply(parts, `[`, 1))
+      inches <- as.numeric(sapply(parts, `[`, 2))
+      feet * 12 + inches
+    },
+    as.numeric(ft_in_vec) * 12
   )
-  return(total_inches)
+  
+  return(result)
 }
-
 
 # 6015 height notation
 parse_height_notation <- function(height_str) {
@@ -35,37 +39,69 @@ parse_height_notation <- function(height_str) {
   return(height_inches)
 }
 
+# 6.1 height notation (accidental)
+parse_feet_decimal_notation <- function(length_vec) {
+  length_vec <- as.character(length_vec)
+  
+  is_decimal <- grepl("\\.", length_vec)
+  
+  result <- ifelse(
+    is_decimal,
+    {
+      parts <- strsplit(length_vec[is_decimal], "\\.")
+      feet <- as.numeric(sapply(parts, `[`, 1))
+      inches <- as.numeric(sapply(parts, `[`, 2))
+      feet * 12 + inches
+    },
+    as.numeric(length_vec) * 12
+  )
+  
+  return(result)
+}
+
 
 #### Raw data ####
-max_20 <- read_csv("data/2020_maxes_wide.csv")
+max_20 <- read_csv("data/2020_maxes_wide.csv") 
 max_22 <- read_csv("data/spring_2022_maxes_long.csv") 
-max_23 <- read_csv("data/2023_maxes_long.csv") 
+max_23 <- read_csv("data/2023_maxes_long.csv")
 max_24 <- read_csv("data/spring_2024_maxes_long.csv", col_select = c(Athlete, Position, Exercise, Max)) 
-max_24_offseason <- read_csv("data/spring_2024_offseason_bests.csv")
+max_24_offseason <- read_csv("data/spring_2024_offseason_bests.csv") 
 
-#### Clean data ####
+#### Clean data #### (No 2021 data because Covid)
 max_20_clean <- max_20 %>%
-  select(-`...11`, -`...12`, -`...13`, -inches, -`5-10-5`, -`3 Cone`) %>%
-  mutate(across(c(wing, broad_jump), parse_ft_in),
-         year = 2020)
-  
-# No 2021 data because Covid
+  mutate(across(where(is.character), ~ gsub("[‘’]", "'", .)),
+         across(
+           -c(name, position),
+           ~ ifelse(grepl("[A-Za-z]", .), NA, .)
+         ),
+         across(c(wing, broad_jump), parse_ft_in),
+         year = 2020) %>%
+  select(-`...11`, -`...12`, -`...13`, -inches, -`5-10-5`, -`3 Cone`) 
 
 max_22_clean <- max_22 %>%
+  mutate(across(where(is.character), ~ gsub("[‘’]", "'", .)),
+         across(
+           -c(Athlete, Position),
+           ~ ifelse(grepl("[A-Za-z]", .), NA, .)
+         )) %>% 
   mutate(name = tolower(Athlete),
-         broad_jump = parse_ft_in(Broad_Jump),
+         across(c(Height, Wing, Weight, Pre_225, Bench, Squat, Deadlift, Total,
+                  `10yd_Official`, Vertical_Jump), as.numeric),
+         broad_jump = case_when(
+           grepl("\\.", Broad_Jump) | as.numeric(Broad_Jump) < 40 ~ parse_feet_decimal_notation(Broad_Jump), # Two different notations present: 6'1 and 6.1. These mean the same thing.
+           grepl("'", Broad_Jump) ~ parse_ft_in(Broad_Jump),
+           TRUE ~ as.numeric(Broad_Jump)
+         ),
          height = parse_height_notation(Height),
          height = ifelse(name == "maurcus mcdaniel", 72, height),
-         across(c(Height, Wing, Weight, Pre_225, Bench, Squat, Deadlift, Total,
-                         `10yd_Official`, Vertical_Jump, Broad_Jump), as.numeric),
          bench_225 = case_when(
            Pre_225 > 0 ~ Pre_225,
-           Pre_225 == 0 & Bench >= 225 ~ NA_real_,
+           Pre_225 == 0 & (Bench >= 225 | is.na(Bench)) ~ NA_real_,
            Pre_225 == 0 & Bench < 225 ~ 0,
            TRUE ~ Pre_225
          )) %>%
-  select(name, height, wing = Wing, weight = Weight, bench_225, bench = Bench, squat = Squat,
-         sprint_10y = `10yd_Official`, vertical_jump = Vertical_Jump, broad_jump, position = Position) %>%
+  select(name, position = Position, height, wing = Wing, weight = Weight, bench_225, bench = Bench, squat = Squat,
+         sprint_10y = `10yd_Official`, vertical_jump = Vertical_Jump, broad_jump) %>%
   mutate(year = 2022)
 
 max_23_clean <- max_23 %>%
@@ -90,15 +126,23 @@ max_23_clean <- max_23 %>%
   unique() %>%
   pivot_wider(names_from = Test, values_from = Best) %>%
   as.data.frame() %>%
-  select(-Name) %>%
-  rename(position = Position, sprint_10y = `10 yard sprint`, height = Height, vertical_jump = `Vertical Jump`, 
+  mutate(across(
+    -c(Name, Position),
+    ~ ifelse(grepl("[A-Za-z]", .), NA, .)),
+    Height = ifelse(Height == 0, NA, Height),
+    Wingspan = ifelse(Wingspan == 0, NA, Wingspan),
+    `225 Bench` = ifelse(`225 Bench` == 0, NA, `225 Bench`)) %>%
+  rename(name = Name, position = Position, sprint_10y = `10 yard sprint`, height = Height, vertical_jump = `Vertical Jump`, 
          weight = Weight, bench_225 = `225 Bench`, bench = `BB Bench`, squat = `Back Squat`, broad_jump = `Broad Jump`, 
          L_drill = `L-Drill`, pro_agility = `Pro Agility`, hang_clean = `Hang Clean`, wing = Wingspan) %>%
   mutate(year = 2023)
 
   
 max_24_offseason_clean <- max_24_offseason %>%
-  mutate(athlete = tolower(athlete),
+  mutate(across(
+    -c(athlete, position),
+    ~ ifelse(grepl("[A-Za-z]", .), NA, .)),
+    athlete = tolower(athlete),
          athlete = str_remove_all(athlete, "'"),
          athlete = str_replace_all(athlete, "ttt", "tt"),
          athlete = str_replace_all(athlete, "narisse", "narcisse"),
@@ -122,14 +166,16 @@ max_24_offseason_clean <- max_24_offseason %>%
                   vert_mat, bench_225, shuttle_60y, L_drill, pro_agil), ~ifelse(. == 0, NA, .)),
          across(c(wing, weight, height, flying_10, `10y_sprint`, vert_vertec, broad_jump,
                   vert_mat, bench_225, shuttle_60y, L_drill, pro_agil), as.numeric)) %>%
-  select(athlete, position, wing, weight, height, flying_10, `10y_sprint`, broad_jump, vert_mat, 
+  select(name = athlete, position, wing, weight, height, flying_10, `10y_sprint`, broad_jump, vert_mat, 
          bench_225, shuttle_60y, L_drill, pro_agil) %>%
   filter(!is.na(athlete)) %>%
   rename(sprint_10y = `10y_sprint`, pro_agility = pro_agil, vertical_jump = vert_mat)
 
 
-max_24_season_clean <- max_24 %>%
-  mutate(Max = as.numeric(Max),
+max_24_season_clean <- max_24 %>% 
+  mutate(Max = ifelse(grepl("[A-Za-z]", Max), NA, Max),
+         Max = ifelse(Max == "?", NA, Max),
+         Max = as.numeric(Max),
          Athlete = tolower(Athlete),
          Athlete = str_remove_all(Athlete, "'"),
          Athlete = str_remove_all(Athlete, " jr"),
@@ -152,12 +198,14 @@ max_24_season_clean <- max_24 %>%
 
 max_24_clean <- max_24_season_clean %>%
   full_join(max_24_offseason_clean, by = c("name_match" = "athlete", "position")) %>%
-  select(-name_match) %>%
+  rename(name = name_match) %>%
   mutate(year = 2024)
 
 #### Merge years #### 
 # Merge each year's cleaned max data
-max_all_clean = bind_rows(max_20_clean, max_22_clean, max_23_clean, max_24_clean) 
+max_all_clean = bind_rows(max_20_clean, max_22_clean, max_23_clean, max_24_clean) %>%
+  select(-name)
+
 
 names(max_all_clean) <- c("Height", "Position", "Wingspan", "Weight", "225lb Bench", "Bench", 
                           "Squat", "Vertical Jump", "Broad Jump", "10-Yard Sprint", "Year", 
